f7e4479e001ce53207b032950ac4b7629f602036
marising <lihaibo@vip.126.com>
2020-03-24 22:07:53 +0800

    revmove interface PBackendService's attachmentHandler

--- fe/src/main/java/org/apache/doris/rpc/PBackendService.java
+++ fe/src/main/java/org/apache/doris/rpc/PBackendService.java
@@ -47,16 +47,13 @@ public interface PBackendService {
             attachmentHandler = ThriftClientAttachmentHandler.class, onceTalkTimeout = 86400000)
     Future<PFetchDataResult> fetchDataAsync(PFetchDataRequest request);
 
-    @ProtobufRPC(serviceName = "PBackendService", methodName = "update_cache",
-            attachmentHandler = ThriftClientAttachmentHandler.class, onceTalkTimeout = 10000)
+    @ProtobufRPC(serviceName = "PBackendService", methodName = "update_cache", onceTalkTimeout = 10000)
     Future<PUpdateCacheResult>  updateCache(PUpdateCacheRequest request);
 
-    @ProtobufRPC(serviceName = "PBackendService", methodName = "fetch_cache",
-            attachmentHandler = ThriftClientAttachmentHandler.class, onceTalkTimeout = 10000)
+    @ProtobufRPC(serviceName = "PBackendService", methodName = "fetch_cache", onceTalkTimeout = 10000)
     Future<PFetchCacheResult> fetchCache(PFetchCacheRequest request);
 
-    @ProtobufRPC(serviceName = "PBackendService", methodName = "clear_cache",
-            attachmentHandler = ThriftClientAttachmentHandler.class, onceTalkTimeout = 10000)
+    @ProtobufRPC(serviceName = "PBackendService", methodName = "clear_cache", onceTalkTimeout = 10000)
     Future<PUpdateCacheResult> clearCache(PUpdateCacheRequest request);
 
     @ProtobufRPC(serviceName = "PBackendService", methodName = "trigger_profile_report",
